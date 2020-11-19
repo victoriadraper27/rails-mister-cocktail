1d884ed8be5bf64a50133378dbd503e87b6c1ef4
Victoria Draper <victoria.draper27@gmail.com>
2020-11-19 12:59:45 +0900

    ARCH: Cocktails Contorller, Routes & Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating Ingredient...'
+
+response = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+  json = JSON.parse(response.read)
+
+json['drinks'].each do |ingre|
+  ingredients = {
+    name: ingre['strIngredient1']
+  }
+  ingredient = Ingredient.create!(ingredients)
+
+  puts "Created #{ingredient.name}"
+end
+
+puts "Finished! Created #{Ingredient.count} ingredients"
